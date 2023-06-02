a80268dfb82cdaa4070f1ddd107b4f267366d42f
jemqoo <120272062+jemqoo@users.noreply.github.com>
2023-06-02 23:47:31 +0300

    Delete p15 directory

--- p15/src/main/java/com/example/p15/utils/MarketNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.p15.utils;
-
-public class MarketNotFoundException extends Exception {
-    public MarketNotFoundException() {
-        super("Market not found");
-    }
-}
--- p15/src/main/java/com/example/p15/utils/WrongMarketDataException.java
+++
@@ -1,7 +0,0 @@
-package com.example.p15.utils;
-
-public class WrongMarketDataException extends Exception{
-    public WrongMarketDataException(String message) {
-        super(message);
-    }
-}
